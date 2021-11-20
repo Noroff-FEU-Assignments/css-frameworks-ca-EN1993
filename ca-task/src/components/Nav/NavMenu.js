f487e586dcd0040d3a1dcd787ae3318eaa6856d9
EN1993 <74547465+EN1993@users.noreply.github.com>
2021-11-20 23:16:03 +0100

    add

--- ca-task/src/components/Nav/NavMenu.js
+++ ca-task/src/components/Nav/NavMenu.js
@@ -18,7 +18,7 @@ function NavMenu() {
           <Container>
             <Navbar bg="light" expand="lg">
               <Container fluid>
-                <Navbar.Brand href="#">The YAY Company</Navbar.Brand>
+                <Navbar.Brand href="/">The YAY Company</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                   <Nav className="me-auto my-2 my-lg-0"
